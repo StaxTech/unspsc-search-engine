cfd52300f55dfc039b51d3184a4b1b384683f9a0
DanfordGidraph <danfordmaina@gmail.com>
2024-04-23 18:44:52 +0300

    * feat(home): add new page component for home page

---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import EnginePage from "./index";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen min-w-screen flex-col bg-white items-center p-24">
+      <EnginePage />
+    </main>
+  );
+}
